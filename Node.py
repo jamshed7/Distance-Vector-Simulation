f56eaf9ec80ced583d4570a2681e16193ef6a38f
Jamshed Jahangir <jjdx11@gmail.com>
2020-04-26 23:02:12 -0500

    Implemented update-link

--- Node.py
+++ Node.py
@@ -1,9 +1,13 @@
-import random
+# Name: Jamshed Jahangir
+# UTA ID: 1001366821
+# NetID: jxj6821
 
 class Node():
-    class_All_Nodes = {}
+    # dictionary that contains all the nodes
+    class_All_Nodes = {}    # node id : node object
 
 
+    # a class method to add a node to our class_All_Nodes dictionary
     @classmethod
     def addNode( cls, a_node ) :
         node_id = a_node.id
@@ -11,46 +15,40 @@ class Node():
             raise ValueError( f'Redeclaration of Node {node_id!r}.' )
         cls.class_All_Nodes[ node_id ] = a_node
 
+    # a class method to find a node object given its id
     @classmethod
     def findNode( cls, node_id ) :
         return cls.class_All_Nodes.get( node_id, None )
 
+    # a class method to update the link cost between 2 existing nodes
     @classmethod
-    def showAllNodes(cls):
-        for item in cls.class_All_Nodes:
-            print(f"Node - {item}")
-            thisNode = cls.findNode(item)
-            print(f"Node ID: {thisNode.id}")
-            print(f"neighbors: {thisNode.displayNeighbors()}")
+    def updateLink(cls, node_1_id, node_2_id, distance):
+        node1 = cls.findNode(node_1_id)
+        node2 = cls.findNode(node_2_id)
 
-    @classmethod
-    def breakNode(cls, broken_node_id):
-        for x in Node.class_All_Nodes:
-            node = cls.findNode(x)
-
-            for node_id in self.distanceTable:
-                if node.id == broken_node_id:
-                    self.distanceTable[node_id] = 16
-                else:
-                    if node_id == broken_node_id:
-                        self.distanceTable[broken_node_id] = 16
+        node1.distanceTable[node_2_id] = distance
+        node2.distanceTable[node_1_id] = distance
+        print("-----------------------------------")
+        print(f"Link between Node {node_1_id} and Node {node_2_id} updated. New link cost = {distance}.")
+        print("-----------------------------------")
 
 
+    # init function to setup a node object
     def __init__(self, node_id, neighbors = []):
         self.id = node_id
-        self.neighbors = neighbors
-
-        self.distanceTable = {} # node_id: shortest distance
-        # self.initialDistanceTable = {}
-        self.routingTable = {} # destination node, route to node
+        self.neighbors = neighbors #list of tuples; tuple format -> (neighbor, distance to neighbor)
+        self.distanceTable = {} # Key - node_id: Value - shortest distance to that node
+        self.routingTable = {} # Key - destination node: Value - route to node
 
         # add neighbors to distanceTable
         for neighbor, cost in neighbors:
             self.distanceTable[neighbor] = cost
 
-        Node.addNode(self)
+        Node.addNode(self)  # add newly created Node object to class_All_Nodes dictionary
 
-
+    # function to initialize the distance table
+    #       if a node is not already added (as a neighbor), then set the distance to that node as INFINITY
+    #       if a node is itself, distance to self is 0
     def initializeDistanceTable(self):
         for node_id in Node.class_All_Nodes:
             if node_id not in self.distanceTable:
@@ -58,6 +56,9 @@ class Node():
             self.distanceTable[ self.id ] = 0
 
 
+    # function to initialize routing table
+    #       if destination node is a neighbor, route directly to neighbor
+    #       if destination node is not a neighbor, just route to first available neighbor (Initial state)
     def initializeRoutingTable(self):
         for node_id in Node.class_All_Nodes:
             # if destination node is also neighbor node, send directly
@@ -69,7 +70,7 @@ class Node():
             else:
                 self.routingTable[node_id], _ = self.neighbors[0]
 
-
+    # function that returns the link state table for a given Node in string format
     def displayTables(self):
         res = ""
         res += f"Node {self.id}\n"
@@ -79,6 +80,7 @@ class Node():
         res += "-----------------------------------"
         return res
 
+
     # check if a given node id is my neighbor
     def isNeighbor(self, a_node_id):
         for neighbor_id, cost in self.neighbors:
@@ -86,8 +88,13 @@ class Node():
                 return True
         return False
 
+    # this is the heart of our program
+    # function that implements the Bellman Ford algorithm
+    #       for every node in the topology, distance to that node is obtained as follows:
+    #           distance to destination node = minimum of (my current distance to that node,
+    #                                               my neighbor's distance to the destination node + my cost to reach my neighbor)
+    #
     def updateDistanceTable(self):
-
         for node_id, distance in self.distanceTable.items():
             if node_id == self.id:
                 continue
@@ -96,24 +103,7 @@ class Node():
 
             for neighbor_id, cost in self.neighbors:
                 neighbor = Node.findNode(neighbor_id)
-                if int(self.distanceTable[node_id]) > neighbor.getDistanceToNode(node_id) + int(cost):
-                    self.distanceTable[node_id] = str( neighbor.getDistanceToNode(node_id) + int(cost) )
+                if int( self.distanceTable[node_id] ) > int( neighbor.distanceTable[node_id] ) + int(cost):
+                    self.distanceTable[node_id] = str( int( neighbor.distanceTable[node_id] ) + int(cost) )
                     self.routingTable[node_id] = neighbor_id
-                    #
                     neighbor.updateDistanceTable()
-
-
-    def getDistanceToNode(self, a_node):
-        return int( self.distanceTable[a_node] )
-
-
-
-    def displayNeighbors(self):
-        print(f"This is node {self.id}")
-        if not self.neighbors:
-            print("I do not have any neighbors.\n")
-            return
-
-        for neighbor, cost in self.neighbors:
-            print(f"My neighbor {neighbor} is {cost} distance away.")
-        print("\n")
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
+# Name: Jamshed Jahangir
+# UTA ID: 1001366821
+# NetID: jxj6821
+
 import sys
 import subprocess
 import platform
 from Node import Node
 
-allNodeIds = []
-
+allNodeIds = [] # node ids are numbers representing a node
+                # this list holds all the node ids
 def _main( fileName ) :
+    # open the fileName that is outlined in the command
 
-    topology = {}
-    # allNodeIds = set()
-    # allNodeIds = []
+    topology = {} # a dictionary to hold a node and corresponding neighbor data
 
     with open( fileName, 'r' ) as fp :
         lines = fp.read().replace('\r', '' ).split( '\n' )
         for line in lines:
             if ( line == '' ) : #ignore blank lines
                 continue
-
+            # split each line and store them as follows:
             from_, to_, cost = line.split()
 
-            # create a dictionary to hold node and corresponding neighbor data
+            # populate the topology dictionary with network information from the file
             if from_ not in topology:
                 topology[from_] = [ (to_, cost) ]
             else:
@@ -36,31 +39,35 @@ def _main( fileName ) :
         # add node id's to list of all nodes
         if node_id not in allNodeIds:
             allNodeIds.append(node_id)
-        newNode = Node(node_id, neighbors)
+        newNode = Node(node_id, neighbors) # create new node
 
-    # sort list of nodes
+    # sort list of node ids
     allNodeIds.sort()
-
-    # initialize distance table for all nodes
+    print("# DEBUG: ")
+    print(topology)
+    # initialize distance and routing table for all nodes
     for node_id in allNodeIds:
         node = Node.findNode(node_id)
         node.initializeDistanceTable()
-        node.initializeRoutingTable()  ## new
+        node.initializeRoutingTable()
 
+    # print welcome prompt
     print("\nWelcome, this program will simulate the Distance-Vector Routing Protocol.\nPlease select your desired run mode.\n")
     print("( 1 ) Auto-mode")
     print("( 2 ) Step-mode")
 
+    # start INFINITE loop; wait until valid input is received from user
     while True:
         modeSelect = input("Enter number to select mode.\n")
-
+        # if user selects option 1 -> run autoMode
         if modeSelect == '1':
             autoMode()
             break
+        # if user selects option 2 -> run stepMode
         elif modeSelect == '2':
             stepMode()
             break
-        else:
+        else: # invalid input received
             print("Error: Please enter a valid number. \nEnter '1' for auto-mode, or '2' for step-by-step mode.")
 
 
@@ -70,28 +77,30 @@ def autoMode():
     print("\nPerforming simulation in Auto Mode")
 
     loopCount = 1
-    curr_state = 'default'
-    last_state = 'default'
+    curr_state = 'default' # variable to keep track of the last state of the network
+    last_state = 'default' # variable to store current state of the network
 
+    # keep looping until stable state is reached
     while True:
-        # curr_node = str( loopCount % len(allNodeIds) )
-        # if( curr_node == '0'):
-        #     curr_node = str( len(allNodeIds) )
+        # select arbitrary node and make it update its link state table
+        # curr_node = allNodeIds[0]
         curr_node = '1'
         node = Node.findNode( curr_node )
         node.updateDistanceTable()
 
         curr_state = ''
-        # show distances from each node
+        # get link state tables from each node and store it in curr_state
         for node_id in allNodeIds:
             node = Node.findNode(node_id)
             curr_state += node.displayTables()
             curr_state += '\n'
 
+        # if curr_state is different from last state then new changes have occured
+        #       increase loop count and restart
         if curr_state != last_state:
             last_state = curr_state
             loopCount +=1
-        else:
+        else: # current state == last state i.e. no changes -> stable state reached
             break
 
     print("===================================")
@@ -99,11 +108,12 @@ def autoMode():
     print("===================================")
 
     stable_state = ''
+    # gather table data again from all nodes and store in stable_state
     for node_id in allNodeIds:
         node = Node.findNode(node_id)
         stable_state += node.displayTables()
         stable_state += '\n'
-    print(stable_state)
+    print(stable_state) # print all tables
 
 
 
@@ -123,37 +133,47 @@ def stepMode():
         print("===================================")
 
         curr_state = ''
-        # show distances from each node
+        # get link state tables from each node and store it in curr_state
         for node_id in allNodeIds:
             node = Node.findNode(node_id)
             curr_state += node.displayTables()
             curr_state += '\n'
-
+        # if curr_state is different from last state then new changes have occured; show current table state
         if curr_state != last_state:
             print( curr_state )
             last_state = curr_state
-            userInput = input("\nType \'quit\' to exit or press return key to keep iterating:\n")
+
         else:
             print("===================================")
             print("Stable State has been reached!")
             print("===================================")
+            # print("To update link information, type 'update link'\n")
 
-            userInput = input("\nType \'quit\' to exit or type 'break [node]' to simulate broken router:\n")
+        # wait for user input
+        print("\nTo update link information, type 'update link'\n")
+        userInput = input("\nType 'quit' to exit or press return key to keep iterating.\n")
 
-
-
+        # user can choose to quit, continue iterating, or change a link cost
         if userInput == 'quit':
             break
-        else:
-            iteration +=1
-            # curr_node = str(iteration % len(allNodeIds) )
-            # if( curr_node == '0'):
-            #     curr_node = str( len(allNodeIds) )
-            curr_node = '1'
 
-            # print (f"\nCurrent node being updated is Node - {curr_node}\n")
-            node = Node.findNode( curr_node )
-            node.updateDistanceTable()
+        if userInput == 'update link':
+            # if user decides to update a link, get new link information from user
+            print("Please type in updated link information\n")
+            print("Format-> [node] [node] [distance]\n")
+            newLink = input()
+            node_1_id, node_2_id, distance = newLink.split()
+            # call the class method updateLink() to implement the changes
+            Node.updateLink(node_1_id, node_2_id, distance)
+            last_state = ''
+            continue
+
+
+        iteration +=1
+        curr_node = allNodeIds[0]
+        # select arbitrary node and make it update its link state table
+        node = Node.findNode( curr_node )
+        node.updateDistanceTable()
 
 
 
@@ -163,7 +183,7 @@ def clear():
     subprocess.Popen( "cls" if platform.system() == "Windows" else "clear", shell=True)
 
 if __name__ == '__main__' :
-  if len( sys.argv ) != 2 :
+  if len( sys.argv ) != 2 : # Handle situation where user does not include input file name
     raise ValueError( 'Please include input file name in command line arguments.' )
 
   _main( sys.argv[1] )
